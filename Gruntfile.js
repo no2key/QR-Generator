d1fceb71531c7cab1219189e74cd71d6c47e02a8
miaojian <kitemao@gmail.com>
2014-07-28 17:33:42 +0800

    polish
    Signed-off-by: miaojian <kitemao@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,7 +76,6 @@ module.exports = function (grunt) {
                     src : [
                         '**/*',
                         '!.git/*',
-                        '!.domino*',
                         '!.editorconfig',
                         '!.git*',
                         '!.travis.yml',
